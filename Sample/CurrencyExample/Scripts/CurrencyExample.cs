565e52c9300f7fee71b94531e816a680094d4410
Konstantin Gindemit <konstantin@getready.io>
2023-03-07 18:04:18 +0000

    New package version for Currency, version: 0.1.0-dev.107

---
+++ Sample/CurrencyExample/Scripts/CurrencyExample.cs
@@ -0,0 +1,12 @@
+using RGN.Impl.Firebase;
+using RGN.UI;
+using UnityEngine;
+
+namespace RGN.Samples
+{
+    public class CurrencyExample : IUIScreen
+    {
+        [SerializeField] private LoadingIndicator _fullScreenLoadingIndicator;
+
+    }
+}
